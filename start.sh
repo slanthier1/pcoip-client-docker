04aa5cb9a0e8784065f1b09d522e652f055cbffc
Jesse Spielman <jesse.spielman@gmail.com>
2020-08-04 00:09:20 +0100

    Split build and run

--- start.sh
+++ start.sh
@@ -2,11 +2,13 @@
 
 SCRIPT_PATH=$(dirname "$(realpath $0)")
 
+docker build -t pcoip_appliance .
+
 docker run --rm -it -d \
   -e DISPLAY=$DISPLAY \
   -e QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v teradici-client-config:/home/myuser/.config \
   --network=host \
-  --device=/dev/dri/:/dev/dri/:rw \
-  `docker build -f ${SCRIPT_PATH}/Dockerfile ${SCRIPT_PATH}`
+  --device=/dev/dri/:/dev/dri/ \
+  pcoip_appliance
