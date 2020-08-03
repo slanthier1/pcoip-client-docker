55fe084bf0b6c2d2a5d9a71db4a2b5a288cc71be
Jesse Spielman <jesse.spielman@gmail.com>
2020-08-04 00:09:40 +0100

    fix indentation

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ SCRIPT_PATH=$(dirname "$(realpath $0)")
 docker build -t pcoip_appliance .
 
 docker run --rm -it -d \
-  -e DISPLAY=$DISPLAY \
-  -e QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v teradici-client-config:/home/myuser/.config \
-  --network=host \
-  --device=/dev/dri/:/dev/dri/ \
-  pcoip_appliance
+    -e DISPLAY=$DISPLAY \
+    -e QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v teradici-client-config:/home/myuser/.config \
+    --network=host \
+    --device=/dev/dri/:/dev/dri/ \
+    pcoip_appliance
