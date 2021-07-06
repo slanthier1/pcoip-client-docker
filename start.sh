bb3cd2fbdea4b09e5e52792aef86c797771b7431
Jesse Spielman <jesse.spielman@gmail.com>
2021-07-06 15:36:55 +0100

    Store logs!

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ sudo docker run --rm -it -d \
     -e QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v `pwd`/teradici-client-config/:/home/`whoami`/.config/Teradici/ \
+    -v `pwd`/logs:/tmp/Teradici/`whoami`/PCoIPClient/logs \
     --network=host \
     --device=/dev/dri/:/dev/dri/ \
     --device=/dev/bus/usb:/dev/bus/usb \
